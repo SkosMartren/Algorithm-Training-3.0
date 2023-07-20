bae4617c698b10fb38194e43fe9aca6b3b1a2c39
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-20 23:11:31 +0300

    Update my_code.cpp

--- Дивизион B/Обход графов в ширину/36. Длина кратчайшего пути/my_code.cpp
+++ Дивизион B/Обход графов в ширину/36. Длина кратчайшего пути/my_code.cpp
@@ -12,7 +12,6 @@ using namespace std;
 Если моделировать этот процесс, то за каждую итерацию алгоритма будет происходить расширение «кольца огня» в ширину на единицу. 
 Номер шага, на котором вершина v начинает гореть, в точности равен длине её минимального пути из вершины s.
 */
-queue<int> BurningVertexes; // Queue -- содержит вершины из текущего кольца огня и, может быть, последующего 
 
 int N; // N – количество вершин в графе
 
@@ -20,9 +19,13 @@ const int UNDEF = -1; /* UNDEF == локальная бесконечность,
                        В случае, если VertexBegin и вершина v находятся в разных компонентах связности, расстояние 
                        между ними в len будет равно -1 – это можно заметить, например, в «пример 2»*/
 
+int VertexBegin, VertexEnd; // "Далее задаются номера двух вершин – начальной и конечной."
+
 void BFS(const vector <vector<int>>& graph, vector<int>& len) {
 
-
+    queue<int> BurningVertexes; // Queue -- содержит вершины из текущего кольца огня и, может быть, последующего 
+    BurningVertexes.push(VertexBegin); // "на нулевом шаге мы поджигаем вершину s [стартовую вершину, от которой ищем минимальное расстояние]"
+  
     while (!BurningVertexes.empty()) {
 
         const int Current = BurningVertexes.front(); 
@@ -30,8 +33,11 @@ void BFS(const vector <vector<int>>& graph, vector<int>& len) {
 
         for (int next : graph[Current]) { // next -- сосед вершины Current, на нее перекинется огонь с Current, если ранее она не была посещена 
             if (len[next] == UNDEF) { // == Вершина next ни разу не посещена
-                BurningVertexes.push(next);
-                len[next] = len[Current] + 1;
+              
+              len[next] = len[Current] + 1;  
+              if (next == VertexEnd) { return; }
+              BurningVertexes.push(next);
+                
             }
         }
 
@@ -58,13 +64,13 @@ int main() {
     }
 
 
-    int VertexBegin, VertexEnd;
-    cin >> VertexBegin >> VertexEnd; // Далее задаются номера двух вершин – начальной и конечной.
+    
+    cin >> VertexBegin >> VertexEnd; 
     VertexBegin--; VertexEnd--; // номера двух вершин положительны, номеруются от 1 до N, для того чтобы работать с ними в vector необходимо декрементировать их
 
     vector<int> len(N, UNDEF); //  == length == len[i] --> расстояние от вершины VertexBegin до вершины i + 1
     len[VertexBegin] = 0; // расстояние от стартовой вершины до стартовой вершины, очевидно, равно 0
-    BurningVertexes.push(VertexBegin); // "на нулевом шаге мы поджигаем вершину s [стартовую вершину, от которой ищем минимальное расстояние]"
+    
 
     BFS(graph, len);
 
