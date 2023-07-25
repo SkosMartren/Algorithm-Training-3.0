8e7386ab401bfdbbf5d72d7e7c7fab757b13724d
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2023-07-25 03:00:19 +0300

    Update my_code_upd_Floyd–Warshall.cpp

--- Дивизион B/Обход графов в ширину/36. Длина кратчайшего пути/my_code_upd_Floyd–Warshall.cpp
+++ Дивизион B/Обход графов в ширину/36. Длина кратчайшего пути/my_code_upd_Floyd–Warshall.cpp
@@ -59,3 +59,13 @@ int main() {
     cout << ((AdjacencyMatrix[VertexBegin][VertexEnd] == numeric_limits<int>::max()) ? -1 : AdjacencyMatrix[VertexBegin][VertexEnd]);
 
 }
+// Данный код отработает на тесте:
+/*
+4
+0   8   0   1
+0   0   1   0
+4   0   0   0
+0   2   9   0
+3   2
+*/
+// в то время как my_code_Floyd–Warshall.cpp -- нет
